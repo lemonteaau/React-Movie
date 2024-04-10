2c15c201d338e40345965883a7751168e832e37b
Terry Cheng <cyr062599@gmail.com>
2024-04-10 20:24:42 +0930

    Add PropTypes to StarRating component and refactor code for better readability

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={5} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -11,6 +22,12 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "OK", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="grey" className="test" defaultRating={2} />
+
+    <Test />
   </StrictMode>
 );
