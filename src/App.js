4ad2d3999fc164d1406249beb94147b101a2cf26
Terry Cheng <cyr062599@gmail.com>
2024-04-19 22:55:15 +0930

    Fix focus issue and handle Enter key press in Search component

--- src/App.js
+++ src/App.js
@@ -169,9 +169,23 @@ function Logo() {
 function Search({ query, setQuery }) {
   const inputElement = useRef(null);
 
-  useEffect(function () {
-    inputElement.current.focus();
-  }, []);
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputElement.current) {
+          return;
+        }
+
+        if (e.code === "Enter") {
+          inputElement.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
   // useEffect(function () {
   //   const el = document.querySelector(".search");
   //   console.log(el);
